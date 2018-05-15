d18deebb9d0379bd6fa03705b4ca531f0db71de8
diAz <zack.bouvy@gmail.com>
2018-05-15 20:02:54 +0200

    1.1.0

--- index.js
+++ index.js
@@ -1,33 +1,82 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const botPrefix = "!lb"
+var compos = require('./data/compos.json');
+var members = require('./data/members.json');
 
-const compos = [{ "name" : "Hard Engage", "characters": "Aatrox, Amumu, Fizz, Miss fortune, Zyra" },
-{ "name" : "Hard Engage", "characters": "Gnar, Jarvan IV, Lissandra, Tristana, Janna" },
-{ "name" : "Hard Engage", "characters": "Gnar, Jarvan IV, Fizz, Lucian, Soraka" },
-{ "name" : "Hard Engage", "characters": "Gnar, Elise, Ahri, Sivir, Alistar" },
-{ "name" : "Hard Engage", "characters": "Maokai, Gragas, Azir, Sivir, Annie" },
-{ "name" : "Yasuo Knockup", "characters": "Yasuo, Gragas, Orianna, Corki, Alistar" },
-{ "name" : "Split Push", "characters": "Jax, Lee, Syndra, Lucian, Braum" },
-{ "name" : "Split Push", "characters": "Ryze, Nunu, Yasuo, Tristana, Morgana" },
-{ "name" : "Orianna Comp", "characters": "Gragas, Rengar, Orianna, Kog maw, Braum" },
-{ "name" : "Orianna Comp", "characters": "Zac, Jarvan IV, Orianna, Ashe, Lulu" },
-{ "name" : "Protect the ADC", "characters": "Lulu, Lee sin, Kassasin, Kog maw, Nami" },
-{ "name" : "Cloud 9", "characters": "Rumble, Elise, Fizz, Ashe, Zyra" },
-{ "name" : "Cloud 9", "characters": "Shen, Zac, Kassadin, Ashe, Zyra" },
-{ "name" : "SKT classico", "characters": "Jax, Lee sin, Zed, Vayne,Zyra" },
-{ "name" : "Sivir comp", "characters": "Gnar, Elise, Ahri, Sivir, Alistar" },
-{ "name" : "Poke comp", "characters": "Ekko, Gragas, Varus, Corki, Janna" },
-{ "name" : "Global comp", "characters": "Shen, Nocturn, Karthus, Twitch, Sona" }]
+const client = new Discord.Client();
+
+var result = ""
+
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
+    result = ""
     switch(msg.content) {
         case '!lb members':
-            msg.channel.send("Simon : Shen/Swain(champion de con)/Gnar/Sion/Mundo/Ornn/Jax\n Samuel: VI ,Zac, warwick, lee sin (re-train un peu),fiddlestick,elise,\n Zack : Brand | Fizz | Orianna\n Gwen :Caitlyn | Jhin | Varus | Ashe | Ezreal\n Valou : champions de type e-girl\nSacha :\nBapt :\n")
+            for(var i in members){
+                result += members[i].name+' - '+members[i].characters+'\n';
+            }
+            msg.channel.send(result)
+        break;
+        case '!lb add compo':
+            msg.channel.send("Quelle est le nom de ta nouvelle compo ? **!name 'nom_de_la_compo'**");
+            const collector = new Discord.MessageCollector(msg.channel, m => m.author.id === msg.author.id, { time: 300000 });
+            console.log(collector)
+            var name = ""
+            var mid = ""
+            var top = ""
+            var adc = ""
+            var supp = ""
+            var jungle = ""
+            collector.on('collect', msg => {
+                if (msg.content.includes("!name")) {
+                    name = msg.content.substring(5);
+                    msg.channel.send("Quelle est le nom du TOP LANER ? **!top 'nom_du_top'**");
+                    collector.on('collect', msg => {
+                        if (msg.content.includes("!top")) {
+                            top = msg.content.substring(4).trim()
+                            msg.channel.send("Quelle est le nom du JUNGLER ? **!jun 'nom_du_jun'**");
+                            collector.on('collect', msg => {
+                                if (msg.content.includes("!jun")) {
+                                    jungle = msg.content.substring(4).trim()
+                                    msg.channel.send("Quelle est le nom du MID ? **!mid 'nom_du_mid'**");
+                                    collector.on('collect', msg => {
+                                        if (msg.content.includes("!mid")) {
+                                            mid = msg.content.substring(4).trim()
+                                            msg.channel.send("Quelle est le nom de l'ADC ? **!adc 'nom_adc'**");
+                                            collector.on('collect', msg => {
+                                                if (msg.content.includes("!adc")) {
+                                                    adc = msg.content.substring(4).trim()
+                                                    msg.channel.send("Quelle est le nom du SUPPORT ? **!supp 'nom_supp'**");
+                                                    collector.on('collect', msg => {
+                                                        if (msg.content.includes("!supp")) {
+                                                            supp = msg.content.substring(5).trim()
+                                                            msg.channel.send("la compo "+ name +" a été enregistrée\n\n[TOP] "+top+"\n[JUNGLE] "+jungle+"\n[MID] "+mid+"\n[ADC] "+adc+"\n[SUPP] "+supp);
+                                                            compos.push({"name" : name, "characters": top+", "+jungle+", "+mid+", "+adc+", "+supp})
+                                                            console.log(compos);
+                                                        }
+                                                    })
+                                                }
+                                            })
+                                        }
+                                    })
+                                }
+                            })
+                        }
+                    })
+                    
+                }
+            })
+            msg.channel.send("")
+
+            
+            var substr = msg.content.substring(14);
+            console.log("bien joué")
+        
+            msg.channel.send(substr)
         break;
         case '!lb ingame':
             msg.channel.send("* Composition de team ( tank , ap , ad , depush )\n* Communication Vocale ( si possible) : position du jungle ennemie, décalage des laners, summoner\n* Vision jungle ennemie : pink wards etc \n-Achat d’une pink ward au premier back \n-Garder les entrées de notre jungle wardées \n-Le jungler garde une pink dans le cas où il veut cheese un objectif \n-support toujour le plus de pink pour mieux dé-wardé \n* Aggression en lane : -information du jg ennemie \n-Gank ou counter Gank \n-débilité de l'ennemie\n* Dragon : - Jg ennemie mort ou topside + pression sur les lanes bot et mid \n* Nash   : - Jg ennemie mort ou botside + une partie de l'équipe ennemie morte , Possibilité de Nash plus risqué si la game est mal partie\n* Focus sur l'adc ou squishy feed \n* Pas de call risqué si on est devant \n* finir la game le plus vite possible ( ne pas troll si on est tous feed ) ")
@@ -36,27 +85,45 @@ client.on('message', msg => {
             msg.channel.send("* On se pose pour réfléchir en fin de partie : qu’est ce qui à été bien ? et pas bien ?\n* Quelle prise de décision a pu déclencher une victoire ? / une défaite ?\n* Quelle sont les axes d’améliorations pour les games futurs\n* Pourquoi pas regarder des replays de temps en temps")
         break;
         case '!lb ban':
-            msg.channel.send("Gwen : Kai’sa , vayne , lb \nSimon: Irelia/(Illaoi ?)\nsamuel: swain maitre yi fizz (zed)")
+            for(var i in members){
+                result += members[i].name+' - '+members[i].ban+'\n';
+            }
+            msg.channel.send(result)
         break;
         case '!lb compo':
-            var composDisplay = '';
-
             for(var i in compos){
-                composDisplay += compos[i].name+' : '+compos[i].characters+'\n';
-                console.log("i : "+ i +" other : "+compos[i].name+' : '+compos[i].characters+'\n')
+                result += compos[i].name+' - '+compos[i].characters+'\n';
             }
-
-            msg.channel.send(composDisplay)
-        
+            msg.channel.send(result)
         break;
         case '!lb train':
-            msg.channel.send("Simon : Irelia et connaître mieux les oppositions au top pour ne pas greed (Train le rôle)\nSamuel , re-train lee sin gragra sejuani et fiddle s'entraîner et peut être popy si nécessaire\nZack : Syndra / Kassadin\nGwen :Xayah | Kai’sa\nValou : des supports de type boje et virils\nSacha :\nBapt :")
+            for(var i in members){
+                result += members[i].name+' - '+members[i].train+'\n';
+            }
+            msg.channel.send(result)
         break;
         case '!lb ?' :
         case '!lb help' :
-            msg.channel.send("List des commandes :\n*members \n*ingame \n*outgame \n*ban \n*compo \n*train \n*help")
-        break;         
+            msg.channel.send("Liste des commandes :\n\n*members \n*ingame \n*outgame \n*ban \n*compo \n*train \n*help")
+        break;       
     }
 });
 
 client.login('NDQ1ODUzMDUzMjcxNDA4NjYw.DdxIIA.6b_4PT-PFwUcZey_VLsOdy6-Cog');
+
+
+function loadJSON(path,callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'path', true); 
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+
+    
