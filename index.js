a28fb831b67116170b6716cc05da22898d7b65c8
diAz <zack.bouvy@gmail.com>
2018-05-17 19:40:50 +0200

    1.1.5.1

--- index.js
+++ index.js
@@ -23,16 +23,41 @@ client.on('message', msg => {
             }
             msg.channel.send(result)
         break;
+        case '!lb ingame':
+            msg.channel.send("* Composition de team ( tank , ap , ad , depush )\n* Communication Vocale ( si possible) : position du jungle ennemie, décalage des laners, summoner\n* Vision jungle ennemie : pink wards etc \n-Achat d’une pink ward au premier back \n-Garder les entrées de notre jungle wardées \n-Le jungler garde une pink dans le cas où il veut cheese un objectif \n-support toujour le plus de pink pour mieux dé-wardé \n* Aggression en lane : -information du jg ennemie \n-Gank ou counter Gank \n-débilité de l'ennemie\n* Dragon : - Jg ennemie mort ou topside + pression sur les lanes bot et mid \n* Nash   : - Jg ennemie mort ou botside + une partie de l'équipe ennemie morte , Possibilité de Nash plus risqué si la game est mal partie\n* Focus sur l'adc ou squishy feed \n* Pas de call risqué si on est devant \n* finir la game le plus vite possible ( ne pas troll si on est tous feed ) ")
+        break;
+        case '!lb outgame':
+            msg.channel.send("* On se pose pour réfléchir en fin de partie : qu’est ce qui à été bien ? et pas bien ?\n* Quelle prise de décision a pu déclencher une victoire ? / une défaite ?\n* Quelle sont les axes d’améliorations pour les games futurs\n* Pourquoi pas regarder des replays de temps en temps")
+        break;
+        case '!lb ban':
+            for(var i in members){
+                result += members[i].name+' - '+members[i].ban+'\n';
+            }
+            msg.channel.send(result)
+        break;
+        case '!lb compos':
+            for(var i in compos){
+                result += '**#'+compos[i].id+'** '+compos[i].name+' - '+compos[i].characters+'\n';
+            }
+            msg.channel.send(result)
+        break;
+        case '!lb train':
+            for(var i in members){
+                result += members[i].name+' - '+members[i].train+'\n';
+            }
+            msg.channel.send(result)
+        break;
+        case '!lb ?' :
+        case '!lb help' :
+            msg.channel.send("Liste des commandes :\n\n*members \n*ingame \n*outgame \n*ban \n*compos \n*add compo \n*train \n*help \n*doc")
+        break;
+        case '!lb doc' :
+            msg.channel.send('https://docs.google.com/document/d/'+key.google+'/edit')
+        break
         case '!lb add compo':
             msg.channel.send("Quelle est le nom de ta nouvelle compo ? **!name 'nom_de_la_compo'**");
             const collector = new Discord.MessageCollector(msg.channel, m => m.author.id === msg.author.id, { time: 40000 });
-            console.log(collector)
-            var name = ""
-            var mid = ""
-            var top = ""
-            var adc = ""
-            var supp = ""
-            var jungle = ""
+            var name,top,jungle,mid,adc,supp = "";
             collector.on('collect', msg => {
                 if (msg.content.includes("!name")) {
                     name = msg.content.substring(5).replace(/\s/g, '');
@@ -73,38 +98,6 @@ client.on('message', msg => {
                 }
             })
         break;
-        
-        case '!lb ingame':
-            msg.channel.send("* Composition de team ( tank , ap , ad , depush )\n* Communication Vocale ( si possible) : position du jungle ennemie, décalage des laners, summoner\n* Vision jungle ennemie : pink wards etc \n-Achat d’une pink ward au premier back \n-Garder les entrées de notre jungle wardées \n-Le jungler garde une pink dans le cas où il veut cheese un objectif \n-support toujour le plus de pink pour mieux dé-wardé \n* Aggression en lane : -information du jg ennemie \n-Gank ou counter Gank \n-débilité de l'ennemie\n* Dragon : - Jg ennemie mort ou topside + pression sur les lanes bot et mid \n* Nash   : - Jg ennemie mort ou botside + une partie de l'équipe ennemie morte , Possibilité de Nash plus risqué si la game est mal partie\n* Focus sur l'adc ou squishy feed \n* Pas de call risqué si on est devant \n* finir la game le plus vite possible ( ne pas troll si on est tous feed ) ")
-        break;
-        case '!lb outgame':
-            msg.channel.send("* On se pose pour réfléchir en fin de partie : qu’est ce qui à été bien ? et pas bien ?\n* Quelle prise de décision a pu déclencher une victoire ? / une défaite ?\n* Quelle sont les axes d’améliorations pour les games futurs\n* Pourquoi pas regarder des replays de temps en temps")
-        break;
-        case '!lb ban':
-            for(var i in members){
-                result += members[i].name+' - '+members[i].ban+'\n';
-            }
-            msg.channel.send(result)
-        break;
-        case '!lb compos':
-            for(var i in compos){
-                result += '**#'+compos[i].id+'** '+compos[i].name+' - '+compos[i].characters+'\n';
-            }
-            msg.channel.send(result)
-        break;
-        case '!lb train':
-            for(var i in members){
-                result += members[i].name+' - '+members[i].train+'\n';
-            }
-            msg.channel.send(result)
-        break;
-        case '!lb ?' :
-        case '!lb help' :
-            msg.channel.send("Liste des commandes :\n\n*members \n*ingame \n*outgame \n*ban \n*compos \n*add compo \n*train \n*help \n*doc")
-        break;
-        case '!lb doc' :
-            msg.channel.send('https://docs.google.com/document/d/'+key.google+'/edit')
-        break
     }
 });
 
